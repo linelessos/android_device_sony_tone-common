95fced0275b9bc667f7c87dd0ec7cb7cbe0242b7
Alin Jerpelea <alin.jerpelea@sony.com>
2018-08-29 16:02:31 +0200

    tz_api_loire.h: set the API for Android 7 firmware as default
    The Loire devices were launched with Android 6 and there was an API change
    for Android 7.
    
    This commit sets as default the new API that came with Android 7
    firmware as default.
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- tz_api_loire.h
+++ tz_api_loire.h
@@ -23,13 +23,23 @@
 extern "C" {
 #endif
 
-#define TZ_RESPONSE_OFFSET 64
+#define FP_TZAPP_PATH "/odm/firmware/"
+#define FP_TZAPP_NAME "tzfingerprint"
+
+#define KM_TZAPP_PATH "/firmware/image/"
+#define KM_TZAPP_NAME "keymaste"
+#define KM_TZAPP_ALT_NAME "keymaster"
+
+#define BUFFER_SIZE 64
+
+#define TZ_RESPONSE_OFFSET 256
 
 enum fingerprint_group_t {
   FPC_GROUP_NORMAL = 0x1,
   FPC_GROUP_DB = 0x2,
   FPC_GROUP_FPCDATA = 0x3,
   FPC_GROUP_DEBUG = 0x6, // I think?
+  FPC_GROUP_QC = 0x07,
 };
 
 //enumerate tz app command ID's
@@ -38,17 +48,17 @@ enum fingerprint_normal_cmd_t {
     FPC_ENROL_STEP = 0x01,
     FPC_END_ENROL = 0x02,
     FPC_IDENTIFY = 0x03,
-    FPC_WAIT_FINGER_LOST = 0x04,
-    FPC_WAIT_FINGER_DOWN = 0x06,
-    FPC_GET_FINGER_STATUS =0x07,
-    FPC_LOAD_EMPTY_DB = 0x09,
-    FPC_GET_FINGERPRINTS = 0x0C,
-    FPC_DELETE_FINGERPRINT = 0x0D,
-    FPC_CAPTURE_IMAGE = 0x0E,
-    FPC_SET_GID = 0x0F,
-    FPC_GET_TEMPLATE_ID = 0x10,
-    FPC_INIT = 0x11,
-    FPC_PRINT_INFO = 0x12,
+    FPC_UPDATE_TEMPLATE = 0x04,
+    FPC_WAIT_FINGER_LOST = 0x05,
+    FPC_WAIT_FINGER_DOWN = 0x07,
+    FPC_GET_FINGER_STATUS =0x8,
+    FPC_LOAD_EMPTY_DB = 0x0A,
+    FPC_GET_FINGERPRINTS = 0xD,
+    FPC_DELETE_FINGERPRINT = 0xE,
+    FPC_CAPTURE_IMAGE = 0xF,
+    FPC_SET_GID = 0x10,
+    FPC_GET_TEMPLATE_ID = 0x11,
+    FPC_INIT = 0x12,
 };
 
 enum fingerprint_fpcdata_cmd_t {
@@ -56,18 +66,23 @@ enum fingerprint_fpcdata_cmd_t {
     FPC_GET_AUTH_CHALLENGE = 0x02,
     FPC_AUTHORIZE_ENROL = 0x03,
     FPC_GET_AUTH_RESULT = 0x04,
-    FPC_SET_KEY_DATA= 0x05,
+    FPC_SET_KEY_DATA = 0x05,
+    FPC_IS_USER_VALID = 0x07,
 };
 
 enum fingerprint_db_cmd_t {
-    FPC_LOAD_DB = 0x0A,
-    FPC_STORE_DB = 0x0B,
+    FPC_LOAD_DB = 0x0B,
+    FPC_STORE_DB = 0x0C,
 };
 
 enum fingerprint_debug_cmd_t {
-   FPC_GET_SENSOR_INFO = 0x03,
+    FPC_GET_SENSOR_INFO = 0x03,
 };
 
+enum fingerprint_qc_cmd_t {
+    FPC_SET_QC_AUTH_NONCE = 0x01,
+    FPC_GET_QC_AUTH_RESULT = 0x02,
+};
 
 #ifdef __cplusplus
 }
