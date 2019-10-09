linelessos/android_device_sony_tone-common

commit 9b250b6c1e14ed6799e85930a93715059704bfea

fpc_imp: Close event fds when freeing data memory.
Otherwise these descriptors stay open unnecessarily.

Change-Id: I6fcd96b7a21c2f65a6bdae3bfd13a75c4cf459ce

fpc_imp_loire_tone.c
@@ -603,6 +603,9 @@ err_t fpc_close(fpc_imp_data_t **data)
 {
     ALOGV(__func__);
     fpc_data_t *ldata = (fpc_data_t*)data;
+
+    fpc_event_destroy(&ldata->data.event);
+
     ldata->qsee_handle->shutdown_app(&ldata->fpc_handle);
     if (fpc_set_power(&(*data)->event, FPC_PWROFF) < 0) {
         ALOGE("Error stopping device\n");

fpc_imp_yoshino_nile_tama.c
@@ -715,6 +715,8 @@ err_t fpc_close(fpc_imp_data_t **data)
 
     fpc_deep_sleep(*data);
 
+    fpc_event_destroy(&ldata->data.event);
+
     ldata->qsee_handle->shutdown_app(&ldata->fpc_handle);
     if (fpc_set_power(&(*data)->event, FPC_PWROFF) < 0) {
         ALOGE("Error stopping device\n");

commit c7995fbe95490615af0ebdad07023758314b6ebe

imp: BUGFIX: Close fingerprint device fd AFTER powering down.
The dev_fd was erroneously closed before being used to turn the device
off through an ioctl, resuting in a "Bad file descriptor". This bug was
introduced in:
9b250b6c1e14ed6799e85930a93715059704bfea

Signed-off-by: Marijn Suijten <marijns95@gmail.com>

common.c
@@ -1,10 +1,10 @@
 #include "common.h"
-#include <string.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include <errno.h>
+#include <fcntl.h>
 #include <poll.h>
+#include <string.h>
 #include <sys/ioctl.h>
+#include <unistd.h>
 #if PLATFORM_SDK_VERSION >= 28
 #include <bits/epoll_event.h>
 #endif
@@ -73,7 +73,7 @@ err_t fpc_set_power(const fpc_event_t *event, int poweron)
 
     ret = ioctl(event->dev_fd, FPC_IOCWPREPARE, poweron);
     if (ret < 0) {
-        ALOGE("Error preparing FPC device\n");
+        ALOGE("Failed preparing FPC device (%d) %s", ret, strerror(errno));
         return -1;
     }
 
@@ -87,7 +87,7 @@ err_t fpc_get_power(const fpc_event_t *event)
 
     ret = ioctl(event->dev_fd, FPC_IOCRPREPARE, &reply);
     if (ret < 0) {
-        ALOGE("Error preparing FPC device\n");
+        ALOGE("Failed reading device power state (%d) %s", ret, strerror(errno));
         return -1;
     }
 

fpc_imp_loire_tone.c
@@ -604,13 +604,14 @@ err_t fpc_close(fpc_imp_data_t **data)
     ALOGV(__func__);
     fpc_data_t *ldata = (fpc_data_t*)data;
 
-    fpc_event_destroy(&ldata->data.event);
-
     ldata->qsee_handle->shutdown_app(&ldata->fpc_handle);
     if (fpc_set_power(&(*data)->event, FPC_PWROFF) < 0) {
         ALOGE("Error stopping device\n");
         return -1;
     }
+
+    fpc_event_destroy(&ldata->data.event);
+
     qsee_free_handle(&ldata->qsee_handle);
     free(ldata);
     *data = NULL;

fpc_imp_yoshino_nile_tama.c
@@ -715,13 +715,14 @@ err_t fpc_close(fpc_imp_data_t **data)
 
     fpc_deep_sleep(*data);
 
-    fpc_event_destroy(&ldata->data.event);
-
     ldata->qsee_handle->shutdown_app(&ldata->fpc_handle);
     if (fpc_set_power(&(*data)->event, FPC_PWROFF) < 0) {
         ALOGE("Error stopping device\n");
         return -1;
     }
+
+    fpc_event_destroy(&ldata->data.event);
+
     qsee_free_handle(&ldata->qsee_handle);
     free(ldata);
     *data = NULL;